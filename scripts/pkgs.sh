a1f42b687f087a0cb2b7424915f60c46362798e5
JuicyGoose007-coder <jaketurner624@gmail.com>
2026-03-26 10:48:36 -0400

    add: scripts folder

---
+++ scripts/pkgs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##########################################################################
+# _____ _   _               _____           _                            #
+#|_   _| | | |             |_   _|         | |  DATE:XX/XX/XXXX          #
+#  | | | |_| | __ ___   _____| | __ _ _ __ | |_ _ __ _   _ _ __ ___  ___ #
+#  | | |  _  |/ _` \ \ / / _ \ |/ _` | '_ \| __| '__| | | | '_ ` _ \/ __|#
+# _| |_| | | | (_| |\ V /  __/ | (_| | | | | |_| |  | |_| | | | | | \__ \#
+# \___/\_| |_/\__,_| \_/ \___\_/\__,_|_| |_|\__|_|   \__,_|_| |_| |_|___/#
+##########################################################################
+# all pkgs
+##########################################################################
+paru -S --noconfirm steam neovim vesktop protonplus faugus-launcher gnome-disk-utility ghostty gamescope ttf-jetbrains-mono-nerd mpv mpv-mpris qbittorrent nwg-look adw-gtk-theme solaar zoxide brave-bin dunst rofi tmux mangohud umu-launcher virt-manager qemu-desktop libvirt edk2-ovmf vlc-plugins-all yt-dlp ffmpegthumbnailer helix lazygit yazi ripgrep pv duf wiremix rust-analyzer vim-plug-git obsidian zen-browser-bin meld matugen stoat-desktop-git claude-code ttf-meslo-nerd networkmanager-openvpn ufw pavucontrol swayidle perl-image-exiftool glances ggshield
+##########################################################################
+# END OF SCRIPT                                                          #
+##########################################################################
