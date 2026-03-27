ad76390f93af91badacafcabe22626a581af7ccd
JuicyGoose007-coder <jaketurner624@gmail.com>
2026-03-27 00:05:44 -0400

    add: install.sh bootstrap script

---
+++ scripts/install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##########################################################################
+# _____ _   _               _____           _                            #
+#|_   _| | | |             |_   _|         | |  DATE:XX/XX/XXXX          #
+#  | | | |_| | __ ___   _____| | __ _ _ __ | |_ _ __ _   _ _ __ ___  ___ #
+#  | | |  _  |/ _` \ \ / / _ \ |/ _` | '_ \| __| '__| | | | '_ ` _ \/ __|#
+# _| |_| | | | (_| |\ V /  __/ | (_| | | | | |_| |  | |_| | | | | | \__ \#
+# \___/\_| |_/\__,_| \_/ \___\_/\__,_|_| |_|\__|_|   \__,_|_| |_| |_|___/#
+##########################################################################
+# install script
+##########################################################################
+
+set -e
+
+REPO="https://github.com/JuicyGoose007-coder/IHT.git"
+DEST="$HOME/IHT"
+
+echo ">> Cloning dotfiles..."
+if [ -d "$DEST" ]; then
+    echo ">> $DEST already exists, pulling latest..."
+    git -C "$DEST" pull
+else
+    git clone "$REPO" "$DEST"
+fi
+
+echo ">> Unpacking dotfiles..."
+cp "$DEST/zshrc" ~/.zshrc
+cp -r "$DEST/fastfetch" ~/.config/
+cp -r "$DEST/ghostty" ~/.config/
+cp -r "$DEST/niri" ~/.config/
+cp -r "$DEST/noctalia" ~/.config/
+cp -r "$DEST/wallpapers" ~/Pictures/
+cp -r "$DEST/swaylock" ~/.config/
+cp -r "$DEST/nvim" ~/.config/
+cp -r "$DEST/yazi" ~/.config/
+cp -r "$DEST/waybar" ~/.config/
+cp -r "$DEST/tmux" ~/.config/
+cp -r "$DEST/dunst" ~/.config/
+cp -r "$DEST/wofi" ~/.config/
+cp -r "$DEST/rofi" ~/.config/
+cp -r "$DEST/wlogout" ~/.config/
+cp -r "$DEST/scripts" ~/
+cp -r "$DEST/Rust" ~/
+
+echo ">> Done! Dotfiles installed."
+
+##########################################################################
+# END OF SCRIPT                                                          #
+##########################################################################
