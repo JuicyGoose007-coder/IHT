288c95f9725e66038c5f6339ede492a926225563
Juicygoose007-coder <jaketurner624@gmail.com>
2026-05-03 17:45:49 -0400

    Wallust

--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -7,27 +7,27 @@
 # _| |_| | | | (_| |\ V /  __/ | (_| | | | | |_| |  | |_| | | | | | \__ \#
 # \___/\_| |_/\__,_| \_/ \___\_/\__,_|_| |_|\__|_|   \__,_|_| |_| |_|___/#
 ##########################################################################
-set -euo pipefail
-
-SRC="${SRC:-$HOME/IHT}"
-
-mkdir -p ~/.config ~/Pictures ~/.config/hypr
-
-cp "$SRC/zshrc" ~/.zshrc
-
-for dir in starship fastfetch ghostty niri swaylock nvim yazi waybar tmux dunst wofi rofi wlogout; do
-  cp -rfT "$SRC/$dir" ~/.config/"$dir"
-done
-
-cp -f "$SRC/hypr/hyprlock.conf" ~/.config/hypr/hyprlock.conf
-
-cp -rfT "$SRC/wallpapers" ~/Pictures/wallpapers
-cp -rfT "$SRC/scripts" ~/scripts
-cp -rfT "$SRC/Rust" ~/Rust
-
-sudo cp "$SRC/sddm.conf" /etc/sddm.conf
-sudo mkdir -p /usr/share/sddm/themes
-sudo cp -rfT "$SRC/oxocarbon-death" /usr/share/sddm/themes/oxocarbon-death
+# base pkgs
+##########################################################################
+cp ~/IHT/zshrc ~/.zshrc
+cp -r ~/IHT/starship ~/.config
+cp -r ~/IHT/fastfetch ~/.config
+cp -r ~/IHT/ghostty ~/.config
+cp -r ~/IHT/niri ~/.config
+cp -r ~/IHT/wallpapers ~/Pictures/wallpapers
+cp -r ~/IHT/swaylock ~/.config
+cp -r ~/IHT/nvim ~/.config
+cp -r ~/IHT/yazi ~/.config
+cp -r ~/IHT/waybar ~/.config
+cp -r ~/IHT/tmux ~/.config
+cp -r ~/IHT/dunst ~/.config
+cp -r ~/IHT/wofi ~/.config
+cp -r ~/IHT/rofi ~/.config
+cp -r ~/IHT/wlogout ~/.config
+cp -r ~/IHT/wallust ~/.config
+cp -r ~/IHT/scripts ~/scripts
+cp -r ~/IHT/Rust ~/Rust
+sudo cp ~/IHT/sddm.conf /etc/sddm.conf
 ##########################################################################
 # END OF SCRIPT                                                          #
 ##########################################################################
